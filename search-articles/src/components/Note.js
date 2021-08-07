7a62a65c5fc3e9b050fa85653ed1220a58cff012
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-07 19:39:37 +0300

    note gets titlesForTable

--- search-articles/src/components/Note.js
+++ search-articles/src/components/Note.js
@@ -2,34 +2,28 @@ import React from "react";
 import { SearchContext } from "../context/searchContext";
 
 export default class Note extends React.Component {
-constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
+  }
 
-}
-
-handleNoteChange (event) {
-
-}
+  handleNoteChange(event) {}
 
   render() {
     return (
       <SearchContext.Consumer>
         {({ titlesForTable }) => (
           <div className="d-flex justify-content-center align-items-center p-2">
-           <div>
-           <p>{}</p>
-           </div> 
-          <div className="form-group m-3">
+            <div>
+              {titlesForTable.map((el) => (
+                <p>{el.article}</p>
+              ))}
+            </div>
+            <div className="form-group m-3">
               <textarea
-              value={}
-              onChange={}
                 className="form-control p-3 m-3 note-box"
                 aria-label="With textarea"
               ></textarea>
-              <button
-                onClick={() => sendTitleToDB()}
-                className="form-control p-2 mx-3 btn btn-outline-secondary"
-              >
+              <button className="form-control p-2 mx-3 btn btn-outline-secondary">
                 Submit
               </button>
             </div>
--- search-articles/src/components/NoteLink.js
+++ search-articles/src/components/NoteLink.js
@@ -4,19 +4,13 @@ import { Link } from "react-router-dom";
 export default class NoteLink extends React.Component {
   render() {
     return (
-      <SearchContext.Consumer>
-      {({}) = (
-<div >
-          <Link to="/note">
-            <div className="d-flex justify-content-center align-items-center p-2 btn btn-outline-secondary"
-            onClick={}>
-              Note
-            </div>
-          </Link>
-        </div>
-      )}
-        
-      </SearchContext.Consumer>
+      <div>
+        <Link to="/note">
+          <div className="d-flex justify-content-center align-items-center p-2 btn btn-outline-secondary">
+            Note
+          </div>
+        </Link>
+      </div>
     );
   }
 }
